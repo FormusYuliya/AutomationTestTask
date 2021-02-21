db073d55b501b9bac37e0b7e890361efdc737c3f
FormusYuliya <75179001+FormusYuliya@users.noreply.github.com>
2021-02-21 23:47:39 +0200

    conflick 1 ix

--- src/main/java/base/DriverFactory.java
+++ src/main/java/base/DriverFactory.java
@@ -1,27 +1,50 @@
 package base;
 
+import org.apache.commons.exec.OS;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.io.File;
+
 public class DriverFactory {
     WebDriver driver;
-    public WebDriver getDriver(String browser){
+    String systemOS = System.getProperty("os.name").toLowerCase();
+    String windowsDrivers = "src" + File.separator + "drivers" + File.separator + "windows" + File.separator;
+    String windowsMac = "src" + File.separator + "drivers" + File.separator + "mac" + File.separator;
+    String windowsLinux = "src" + File.separator + "drivers" + File.separator + "linux" + File.separator;
+
+
+    public WebDriver getDriver(String browser) {
         switch (browser) {
             case ("chrome"):
-                System.setProperty("webdriver.chrome.driver", "src\\drivers\\windows\\chromedriver.exe");
-                driver = new ChromeDriver();
-                System.out.println("CH Driver is launched");
+
+                if (systemOS.contains("win"))  {System.setProperty("webdriver.chrome.driver", windowsDrivers + "chromedriver.exe");}
+                else if(systemOS.contains("mac")) {System.setProperty("webdriver.chrome.driver", windowsMac + "chromedriver");}
+                else if (systemOS.contains("nix")) {System.setProperty("webdriver.chrome.driver", windowsLinux + "chromedriver");}
+
+                    driver = new ChromeDriver();
+//                System.out.println("CH Driver is launched");
                 break;
+
             case ("firefox"):
-                System.setProperty("webdriver.gecko.driver", "src\\drivers\\windows\\geckodriver.exe");
-                driver = new FirefoxDriver();
-                System.out.println("FF Driver is launched");
+
+                if (systemOS.contains("win"))  {System.setProperty("webdriver.gecko.driver", windowsDrivers + "geckodriver.exe");}
+                else if(systemOS.contains("mac")) {System.setProperty("webdriver.gecko.driver", windowsMac + "geckodriver");}
+                else if(systemOS.contains("nix")) {System.setProperty("webdriver.gecko.driver", windowsLinux + "geckodriver");}
+
+
+                    driver = new FirefoxDriver();
+//                System.out.println("FF Driver is launched");
                 break;
+
             default:
                 System.out.println("Can not start driver" + browser);
-                System.setProperty("webdriver.chrome.driver", "src\\drivers\\windows\\chromedriver.exe");
-                driver = new ChromeDriver();
+                if (systemOS.contains("win"))  {System.setProperty("webdriver.chrome.driver", windowsDrivers + "chromedriver.exe");}
+                else if(systemOS.contains("mac")) {System.setProperty("webdriver.chrome.driver", windowsMac + "chromedriver");}
+                else if(systemOS.contains("nix")) {System.setProperty("webdriver.chrome.driver", windowsLinux + "chromedriver");}
+    driver = new ChromeDriver();
+//                System.out.println(System.getProperty("os.name"));
                 break;
         }
         return driver;
